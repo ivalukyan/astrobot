719d4cae6c4116d24db8a3a0f11f2d3c3840dd95
ivanlukanov <ivalukyanov1@gmail.com>
2025-02-10 16:45:49 +0300

    fix google sheets

--- service/google_sheets.py
+++ service/google_sheets.py
@@ -1,8 +1,11 @@
 import asyncio
+import logging
+
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from gspread.exceptions import APIError
 
-# Функция для авторизации (обычный синхронный код)
+# Функция для авторизации
 def authorize():
     scope = [
         "https://spreadsheets.google.com/feeds",
@@ -13,10 +16,18 @@ def authorize():
     client = gspread.authorize(creds)
     return client
 
-# Асинхронный метод для работы с Google Sheets
-async def async_get_sheet():
-    client = await asyncio.to_thread(authorize)  # Запуск в фоновом потоке
-    sheet = await asyncio.to_thread(client.open, "api_test")  # Открываем таблицу
-    worksheet = await asyncio.to_thread(sheet.get_worksheet, 0)
-    return worksheet
-
+# Асинхронный метод для работы с Google Sheets с повторными попытками
+async def async_get_sheet(retries=3, delay=2):
+    for attempt in range(retries):
+        try:
+            client = await asyncio.to_thread(authorize)  # Авторизация
+            sheet = await asyncio.to_thread(client.open, "Контакты")  # Открываем таблицу
+            worksheet = await asyncio.to_thread(sheet.get_worksheet, 0)  # Получаем лист
+            return worksheet  # Успешный результат
+        except APIError as e:
+            if e.response.status_code == 503:  # Ошибка временной недоступности
+                logging.info(f"Google API недоступен. Попытка {attempt + 1}/{retries}")
+                await asyncio.sleep(delay * (2 ** attempt))  # Экспоненциальная задержка
+            else:
+                raise  # Прочие ошибки не обрабатываем
+    logging.info("Google Sheets API недоступен после нескольких попыток.")
